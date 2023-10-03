344912050daef19195986407c79ae952182622d2
tungnt <nguyenthanhtung0207@gmail.com>
2023-10-03 10:18:08 +0700

    add ios & android

---
+++ ios/Classes/FlutterScreenTimePlugin.swift
@@ -0,0 +1,19 @@
+import Flutter
+import UIKit
+
+public class FlutterScreenTimePlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_screen_time", binaryMessenger: registrar.messenger())
+    let instance = FlutterScreenTimePlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+}
